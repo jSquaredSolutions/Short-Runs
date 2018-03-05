3a559517538771bb41cf01a122981c60ea824993
Jeffrey McFarland <jtm27@dhe.duke.edu>
2018-03-05 12:53:41 -0500

    Added Shell Script

---
+++ RaspberryPi/Config.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get update -y
+sudo apt-get autoremove
+sudo apt-get full-upgrade -y
+sudo apt-get update -y
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt install -y nodejs
