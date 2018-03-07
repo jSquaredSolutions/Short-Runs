9eee39a467ff6a55f4b6583834ac1d18b99ac730
Jeffrey McFarland <jtm27@dhe.duke.edu>
2018-03-07 13:50:37 -0500

    shell code

--- RaspberryPi/HyperV-Ubuntu.sh
+++ RaspberryPi/HyperV-Ubuntu.sh
@@ -5,11 +5,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get update -y
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 sudo apt-get full-upgrade -y
 sudo apt-get update -y
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo apt-get update
-sudo apt-get install linux-virtual-lts-xenial
-sudo apt-get install linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial
+sudo apt install -g nodejs -y
+sudo apt-get update -y
+sudo apt-get install linux-virtual-lts-xenial -y
+sudo apt-get install linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial -y
+npm install -g yo gulp -y
+sudo apt-get update -y
+sudo apt-get autoremove -y
