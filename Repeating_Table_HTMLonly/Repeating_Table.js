dc3c925c590eae558584f4da6d318ff1599388bc
Jsquared <info@jsquaredsolutions.net>
2018-09-16 15:43:10 -0400

    More code

--- Repeating_Table_HTMLonly/Repeating_Table.js
+++ Repeating_Table_HTMLonly/Repeating_Table.js
@@ -4,14 +4,16 @@ $(document).ready(function () {
         /*  myObj = { name: "John", city: "New York" };
           myJSON = JSON.stringify(myObj);
           sessionStorage.setItem("testJSON", myJSON); */ 
-          
-
-    
-          
-
-        RepeatingTableObj.TeamMembers.push({Name: $('#GOGname').val(), Position: $('#GOGposition').val(), Agency: $('#GOGagency').val()});
-        var template = "{{#TeamMembers}}<b>{{name}}</b>{{/TeamMembers}}";
+        RepeatingTableObj.TeamMembers.push({Name: $('#GOGname').val(), Position: $('#GOGposition').val(), Agency: $('#GOGagency').val(), ArrayLength: RepeatingTableObj.TeamMembers.length});
+        var template = "{{#TeamMembers}}<div id='{{ArrayLength}}' onclick='helloWorld(this)' style='border-style:solid;border-color:gray'><ul><li>{{Name}}</li><li>{{Position}}</li><li>{{Agency}}</li></ul></div>{{/TeamMembers}}";
         var Output = Mustache.render(template, RepeatingTableObj);
         document.getElementById('TeamMembersOutput').innerHTML = Output;
     });
 });
+
+function helloWorld(elmnt){
+    debugger;
+    elmnt.remove();
+    RepeatingTableObj.TeamMembers.splice(parseInt(elmnt.id),1);
+}
+
