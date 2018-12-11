d1b1fab6deafe0b1aef16fd47eceec8e559817a0
Hawaiinow6$ <info@jsquaredsolutions.net>
2018-12-10 19:33:20 -0500

    Code

---
+++ JS_testing/index.js
@@ -0,0 +1,7 @@
+var myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+
+console.log("HelloWorld");
+console.log(Object.getOwnPropertyNames(myCar));
--- RaspberryPi/HyperV-Ubuntu.sh
+++ RaspberryPi/HyperV-Ubuntu.sh
@@ -1,38 +1,41 @@
 # For Linux Machine to get up and running for SPFx dev Ubuntu
 # elevater perms       chmod 755 HyperV-Ubuntu.sh
-# then use             bash HyperV-Ubuntu.sh
+# then use 'touch setup.sh' in the Ubuntu shell
+# then copy and paste the code there
+# I don't know what linux-virtual-lts-xenial is, I think this might have been for hyper-v screen res
+# sudo apt-get install linux-virtual-lts-xenial -y
+
 sudo apt-get update -y
-sleep 70
+echo "sudo apt-get update -y [DONE]"
 sudo apt-get upgrade -y
-sleep 800
 sudo apt-get dist-upgrade -y
-sleep 200
 sudo apt-get update -y
-sleep 10
 sudo apt-get autoremove -y
-sleep 10
 sudo apt-get full-upgrade -y
-sleep 10
-sudo apt-get install curl -yo
-sleep 10
+sudo apt-get install curl -y
 sudo apt-get update -y
-sleep 10
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sleep 60
 sudo apt install nodejs -y
 sleep 30 
 sudo apt-get update -y
 sleep 60 
-# I don't know what linux-virtual-lts-xenial is, I think this might have been for hyper-v screen res
-sudo apt-get install linux-virtual-lts-xenial -y
-sleep 10
-# I don't know what linux-tools-virtual-lts-xenial is, I think these might be for screen res
-sudo apt-get install linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial -y
-sleep 10
-npm install -g yo gulp -y
+echo "sudo apt-get update -y [DONE]"
 sleep 30
 sudo apt-get update -y
 sleep 15
+echo "sudo apt-get update -y"
 sudo apt-get autoremove -y
-sleep 15
-sudo shutdown -P now
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudp apt-get install git-core
+sudo apt-get update -y
+sudo apt-get purge firefox -y
+sudo apt-get purge thunderbird* -y
+wget http://jsquaredsolutions.net/code_1.29.1-1542309157_amd64.deb
+sudo dpkg -i code_1.29.1-1542309157_amd64.deb
+sudo apt-get update -y
+sudo apt-get autoremove -y
+sudo apt --fix-broken install -y
+sudo apt-get install build-essential -y
+npm install -g yo gulp -y
